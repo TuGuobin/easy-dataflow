d4e8a0b6a69d29b58cc377efe0c6ad23ad413569
TuGuobin <1137725646@qq.com>
2025-10-11 09:36:08 +0800

    refactor(id): change gererate id logic

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -1,5 +1,1 @@
-let counter = 0
-
-export const generateId = (length: number = 2) => {
-  return Math.random().toString(36).substring(2, length + 2) + counter++
-}
+export const generateId = () => crypto.randomUUID();
